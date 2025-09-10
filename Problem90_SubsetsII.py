2a7fd764fa56be37a512db8803de17f08f73799e
vietdai-bk <daithai540@gmail.com>
2025-09-10 09:50:43 +0700

    10/09/2025

---
+++ Problem90_SubsetsII.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res, subRes = [], []
+        def backtrack(start):
+            res.append(subRes.copy())
+            for i in range(start, len(nums)):
+                if i>start and nums[i]==nums[i-1]: continue
+                subRes.append(nums[i])
+                backtrack(i+1)
+                subRes.pop()
+        nums.sort()
+        backtrack(0)
+        return res
+
+if __name__=="__main__":
+    solve = Solution()
+    print(solve.subsetsWithDup([1,2,2]))
