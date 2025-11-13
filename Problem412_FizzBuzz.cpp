490c47b030e3ac97d2b78813a6a00fdd47b0975b
vietdai-bk <daithai540@gmail.com>
2025-11-13 19:17:07 +0700

    13/11/2025

---
+++ Problem412_FizzBuzz.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i=1;i<=n;i++) {
+            if (i%3==0 && i%5==0) res.push_back("FizzBuzz");
+            else if (i%3==0) res.push_back("Fizz");
+            else if (i%5==0) res.push_back("Buzz");
+            else res.push_back(to_string(i));
+        }
+        return res;
+    }
+};
+
+int main() {
+    const int n=3;
+    Solution solve;
+    vector<string> res = solve.fizzBuzz(n);
+    for (string s : res) cout << s << " ";
+}
